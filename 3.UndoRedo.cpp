7403bf20ffb0e7333dd493687e7de464ea7f4557
TrungThachDau <90404593+TrungThachDau@users.noreply.github.com>
2021-10-12 22:38:53 +0700

    Update 3.UndoRedo.cpp

--- 3.UndoRedo.cpp
+++ 3.UndoRedo.cpp
@@ -1,79 +1,140 @@
 #include <iostream>
+#include <string>
 using namespace std;
-struct SNode
+void menu()
 {
-	int data;
-	SNode* next;
+	cout << "\n1. Nhap noi dung: ";
+	cout << "\n2. Xem noi dung";
+	cout << "\n3. Hoan tac";
+	cout << "\n4. Lap lai";
+	cout << "\nChon chuc nang: ";
+}
+struct Node
+{
+	string data;
+	Node* next;
 };
 struct Stack
 {
-	SNode* top;
+	Node* top;
 };
-SNode* CreateNode(int x)
+Node* CreateNode(string x)
 {
-	SNode* p = new SNode;
+	Node* p = new Node;
 	if (p == NULL)
 	{
-		cout << "Not enough ememory  allocated.";
 		return NULL;
 	}
 	p->data = x;
 	p->next = NULL;
 	return p;
 }
-void initStack(Stack& s)
+void init(Stack& s)
 {
 	s.top = NULL;
 }
 bool isEmpty(Stack s)
 {
-
 	if (s.top = NULL)
 	{
 		return true;
 	}
-	else
-	{
-		return false;
-	}
+	return false;
 }
-void push(Stack& s, SNode* p)
+void push(Stack& s, Node* p)
 {
 	if (s.top == NULL)
+	{
 		s.top = p;
+	}
 	else
 	{
 		p->next = s.top;
 		s.top = p;
 	}
 }
-int pop(Stack& s)
+string pop(Stack& s)
 {
 	if (isEmpty(s))
 		return 0;
-	SNode* p = s.top;
-	int data = p->data;
+	Node* p = s.top;
+	string data = p->data;
 	s.top = p->next;
 	delete p;
 	return data;
 }
-int top(Stack s)
+string top(Stack s)
 {
-	if (isEmpty(s))
-		return 0;
 	return s.top->data;
 }
-void printStack(Stack s)
+void print(Stack s)
 {
-	SNode* p = s.top;
+	Node* p = s.top;
 	while (p != NULL)
 	{
 		cout << p->data << " ";
 		p = p->next;
 	}
 }
+void doo(Stack &s1)
+{
+	string c;
+	cout << "\nNhap noi dung: ";
+	cin >> c;
+	Node* p;
+		p = CreateNode(c);
+		push(s1, p);
+	}
+void undo(Stack& s1,Stack&s2)
+{
+	Node* p;
+	p = CreateNode(pop(s1));
+	push(s2, p);
+
+}
+void redo(Stack& s1, Stack &s2)
+{
+	Node* p = CreateNode(pop(s2));
+	push(s1, p);
+}
 int main()
 {
-	Stack s;
-	initStack(s);
+	Stack s1, s2;
+	init(s1);
+	init(s2);
+	int chucnang;
+	do
+	{
+		menu();
+		cin >> chucnang;
+		switch (chucnang)
+		{
+		case 1:
+		{
+			doo(s1);
+			 cout << endl;
+			print(s1); break;
+
+		}
+		case 2:
+		{
+			cout << endl;
+			print(s1); break;
+		}
+		case 3:
+		{
+			undo(s1, s2); cout << endl;
+			print(s1); break;
+		}
+		case 4:
+		{
+			redo(s1, s2); cout << endl;
+			print(s1); break;
+		}
+		default:
+			break;
+		}
+	} while (chucnang!=0);
+	return 0;
 }
+
